981245b3755354e0dc36d40d88417ce1aa6c5f33
xump <2225338553@qq.com>
2024-08-06 22:53:18 +0800

    45贪心

--- 45_跳跃游戏Ⅱ.js
+++ 45_跳跃游戏Ⅱ.js
@@ -3,19 +3,42 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let dp = []
-    dp.push([0,nums[0]])
+    // let dp = []
+    // dp.push([0,nums[0]])
+    // if(nums.length<=1){
+    //     return dp[0][0]
+    // }
+    // dp.push([1,nums[0]-1>1+nums[1]?nums[0]-1:1+nums[1]])
+    // for(let i=1;i<nums.length;i++){
+    //     for(let j=0;j<dp.length;j++){
+    //         if(dp[j][1]>=i){
+    //             dp.push([dp[j][0]+1,i+nums[i]])
+    //             break;
+    //         }
+    //     }
+    // }
+    // return dp[nums.length][0]
     if(nums.length<=1){
-        return dp[0][0]
+        return 0;
     }
-    dp.push([1,nums[0]-1>1+nums[1]?nums[0]-1:1+nums[1]])
-    for(let i=1;i<nums.length;i++){
-        for(let j=0;j<dp.length;j++){
-            if(dp[j][1]>=i){
-                dp.push([dp[j][0]+1,i+nums[i]])
-                break;
+    let maxLen = nums[0];
+    let index = 1;
+    let result = 0;
+    if(maxLen>=nums.length){
+        return 1;
+    }
+    while(maxLen<nums.length-1){
+        let newMax = 0,newIndex=0;
+        while(index<=maxLen){
+            if(index+nums[index]>newMax){
+                newIndex = index;
+                newMax = index+nums[index];
             }
+            index++;
         }
+        result+=1;
+        maxLen = newMax;
+        index = newIndex;
     }
-    return dp[nums.length][0]
+    return result+1;
 };
