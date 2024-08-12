de5486f0d92e4952f4598af8f61761266b4c6a4d
xump <2225338553@qq.com>
2024-08-12 21:42:22 +0800

    15

---
+++ 15_三数之和.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a,b)=>a-b)
+    let result = []
+    let set0 = new Set()
+    for(let i=0;i<nums.length-2;i++){
+        if(set0.has(nums[i])){
+            continue;
+        }
+        else{
+            set0.add(nums[i])
+        }
+        let left = i+1,right=nums.length-1
+        let set = new Set()
+        while(left<right){
+            if(nums[i]+nums[left]+nums[right]>0){
+                right--;
+            }
+            else if(nums[i]+nums[left]+nums[right] === 0){
+                if(set.has(nums[left])){
+                    left++;
+                    continue
+                }
+                else{
+                    set.add(nums[left])
+                }
+                let arr = [nums[i],nums[left],nums[right]]
+                result.push(arr)
+                left++;
+            }
+            else{
+                left++;
+            }
+        }
+    }
+    return result
+};
